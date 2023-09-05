9e845e4fd03cf6a8aa6c526340ef0f0621014972
Ivan Novikov <ivan7237d@gmail.com>
2023-09-05 16:50:54 -0400

    docs

--- packages/core/src/syncReactivity.ts
+++ packages/core/src/syncReactivity.ts
@@ -1,26 +1,26 @@
 /**
  * This module implements synchronous reactivity. You give it a bunch of `() =>
- * void` functions, we'll call them thunks, that have these properties:
+ * void` functions, we'll call them reactions, that have these properties:
  *
  * - When you run one of them, it has a way to indicate whether it has produced
  *   any side effects.
  *
- * - A thunk also has a way to indicate which other thunks are its "direct
+ * - A reaction also has a way to indicate which other reactions are its "direct
  *   dependencies". A "direct dependency" aka a "parent" is just a concept that
  *   we use for the purposes of the next property:
  *
- * - Thunks stay idempotent until either we get an external notification or a
- *   parent thunk is run and produces side effects.
+ * - Reactions stay idempotent until either we get an external notification or a
+ *   parent reaction is run and produces side effects.
  *
  * - Dependencies are non-cyclical.
  *
  * The job of this module is to react to external notifications by running
- * thunks in such a way that:
+ * reactions in such a way that:
  *
- * - Your program ends up in a "stable" state, meaning that after we're done,
- *   running any of the thunks would not produce any side effects.
+ * - Each reaction ends up in a "clean" state, meaning that running it would not
+ *   produce any side effects.
  *
- * - Each thunk is run at most once.
+ * - Each reaction is run at most once.
  *
  * We solve the diamond problem using [three-colors
  * algorithm](https://dev.to/modderme123/super-charging-fine-grained-reactive-performance-47ph).
