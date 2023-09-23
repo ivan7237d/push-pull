1ebb70bc95feb962c7d68f5e61fa276ebb30af19
Ivan Novikov <ivan7237d@gmail.com>
2023-09-23 11:38:34 -0400

    clean up

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,2 @@
-export { createAsyncConst } from "./createAsyncConst";
-export type { AsyncConst } from "./createAsyncConst";
-export { createAsyncVar, isAsync } from "./createAsyncVar";
-export type { AsyncVar, Publisher, Subscriber } from "./createAsyncVar";
-export { map } from "./map";
+export { createMemo } from "./memo";
+export { createSignal } from "./signal";
--- packages/core/src/normalizeNeverAsync.ts
+++
@@ -1,6 +0,0 @@
-import { AsyncConst } from "./createAsyncConst";
-import { AsyncVar } from "./createAsyncVar";
-
-export type NormalizeNeverAsync<Arg> = Arg extends AsyncVar<never, infer Error>
-  ? AsyncConst<never, Error>
-  : Arg;
