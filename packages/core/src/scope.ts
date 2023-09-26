58027ea2aa75187d0cb2d3f9f29f5b332dfeaf2a
Ivan Novikov <ivan7237d@gmail.com>
2023-09-26 11:22:52 -0400

    scope

--- packages/core/src/scope.ts
+++ packages/core/src/scope.ts
@@ -1,13 +1,22 @@
-type Scope = [dispose: () => void, err: () => void];
+interface Scope {}
 
 let currentScope: Scope | undefined;
 
-export const createScope = (
-  callback: () => void,
-  err: (error: unknown) => void
-): Scope => {
-  currentScope;
-  callback;
-  err;
-  throw "not implemented";
+export const getScope = () => currentScope;
+
+export const createScope = (err?: (error: unknown) => void): Scope => {
+  return {};
 };
+
+export const runInScope = <T>(callback: () => T, scope?: Scope): T => {};
+
+export const err = (error: unknown, scope?: Scope) => {};
+
+export const createDisposable = (disposable: () => {}) => {};
+
+export const dispose = (scope: Scope) => {};
+
+export const getContext = <Key extends keyof Scope, DefaultValue = undefined>(
+  key: Key,
+  defaultValue?: DefaultValue
+): Scope[Key] | DefaultValue => {};
