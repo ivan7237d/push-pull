1844976dcb61a58f6f500b552c0180a2f9bd0aa0
Ivan Novikov <ivan7237d@gmail.com>
2023-10-20 11:45:39 -0400

    tests

--- packages/core/src/signal.test.ts
+++ packages/core/src/signal.test.ts
@@ -8,9 +8,11 @@ import { createSignal } from "./signal";
 test("signal", () => {
   runInScope(() => {
     const [signal, setSignal] = createSignal(0);
-    createEffect(() => {
-      log.add(label("effect"))(signal());
-    });
+    runInScope(() => {
+      createEffect(() => {
+        log.add(label("effect"))(signal());
+      });
+    }, createScope());
     expect(readLog()).toMatchInlineSnapshot(`> [effect] 0`);
     setSignal(1);
     expect(readLog()).toMatchInlineSnapshot(`> [effect] 1`);
