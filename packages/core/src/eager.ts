c51fc50d826966adc896884796e2c97e31602ac7
Ivan Novikov <ivan7237d@gmail.com>
2023-10-22 12:52:17 -0400

    eager

--- packages/core/src/eager.ts
+++ packages/core/src/eager.ts
@@ -1,8 +1,11 @@
 import { LazyPromise } from "./lazyPromise";
+import { createRootScope, runInScope } from "./scope";
 
 export const eager = <Value>(
   lazyPromise: LazyPromise<Value, unknown>
 ): Promise<Value> =>
   new Promise((resolve, reject) => {
-    lazyPromise(resolve, reject);
+    runInScope(createRootScope(reject), () => {
+      lazyPromise(resolve, reject);
+    });
   });
