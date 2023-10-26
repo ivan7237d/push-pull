e39c127d3010ed2809fa1af187c00b3b48e53de2
Ivan Novikov <ivan7237d@gmail.com>
2023-10-26 18:17:18 -0400

    lazyPromise tests

--- packages/core/src/lazyPromise.test.ts
+++ packages/core/src/lazyPromise.test.ts
@@ -4,30 +4,6 @@ import { createLazyPromise } from "./lazyPromise";
 import { createScope, runInScope } from "./scope";
 import { log } from "./setupTests";
 
-test("resolve", () => {
-  let resolve: (value: string) => void;
-  const promise = createLazyPromise<string>((newResolve) => {
-    resolve = newResolve;
-  });
-  runInScope(createScope(), () => {
-    promise(log.add(label("resolve")));
-  });
-  resolve!("value");
-  expect(readLog()).toMatchInlineSnapshot(`> [resolve] "value"`);
-});
-
-test("reject", () => {
-  let reject: (value: string) => void;
-  const promise = createLazyPromise<unknown, string>((_, newReject) => {
-    reject = newReject;
-  });
-  runInScope(createScope(), () => {
-    promise(undefined, log.add(label("reject")));
-  });
-  reject!("oops");
-  expect(readLog()).toMatchInlineSnapshot(`> [reject] "oops"`);
-});
-
 test("types: erroring promise", () => {
   const promise = createLazyPromise<string, number>(() => {});
   runInScope(createScope(), () => {
@@ -70,4 +46,28 @@ test("types: non-erroring promise", () => {
       () => {}
     );
   });
+});
+
+test("resolve", () => {
+  let resolve: (value: string) => void;
+  const promise = createLazyPromise<string>((newResolve) => {
+    resolve = newResolve;
+  });
+  runInScope(createScope(), () => {
+    promise(log.add(label("resolve")));
+  });
+  resolve!("value");
+  expect(readLog()).toMatchInlineSnapshot(`> [resolve] "value"`);
+});
+
+test("reject", () => {
+  let reject: (value: string) => void;
+  const promise = createLazyPromise<unknown, string>((_, newReject) => {
+    reject = newReject;
+  });
+  runInScope(createScope(), () => {
+    promise(undefined, log.add(label("reject")));
+  });
+  reject!("oops");
+  expect(readLog()).toMatchInlineSnapshot(`> [reject] "oops"`);
 });
