46c43a6836fb220f1ceed6f5f7ade61e0cbe13f4
Ivan Novikov <ivan7237d@gmail.com>
2023-10-27 21:25:14 -0400

    index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,17 @@
+export { eager } from "./async/eager";
+export { lazy } from "./async/lazy";
+export { createLazyPromise, isLazyPromise } from "./async/lazyPromise";
+export type { LazyPromise, Publisher, Subscriber } from "./async/lazyPromise";
+export { batch, createEffect, pull, push } from "./reactivity";
+export {
+  createRootScope,
+  createScope,
+  disposeScope,
+  getContext,
+  isScopeDisposed,
+  isScopeRunning,
+  onDispose,
+  runInScope,
+} from "./scope";
+export type { Scope } from "./scope";
 export { createSignal } from "./signal";
