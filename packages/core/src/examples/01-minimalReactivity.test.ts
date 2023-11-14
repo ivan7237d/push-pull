529ff33031f0471ead794b175508bb3a08e75d49
Ivan Novikov <ivan7237d@gmail.com>
2023-11-13 22:11:01 -0500

    minimalReactivity

--- packages/core/src/examples/01-minimalReactivity.test.ts
+++ packages/core/src/examples/01-minimalReactivity.test.ts
@@ -8,6 +8,11 @@ import { log } from "../setupTests";
 
 const pullersSymbol = Symbol("pullers");
 const puleesSymbol = Symbol("pulees");
+const sweepersSymbol = Symbol("sweepers");
+const sweepeesSymbol = Symbol("sweepees");
+const colorSymbol = Symbol("color");
+const checkSymbol = Symbol("check");
+const cleanSymbol = Symbol("clean");
 
 interface Subject {
   // eslint-disable-next-line no-use-before-define
@@ -17,6 +22,9 @@ interface Subject {
 interface Reaction {
   (): void;
   [puleesSymbol]?: Subject[];
+  [sweepersSymbol]?: Reaction[];
+  [sweepeesSymbol]?: Reaction[];
+  [colorSymbol]?: typeof checkSymbol | typeof cleanSymbol;
 }
 
 let currentReaction: Reaction | undefined;
@@ -36,14 +44,70 @@ const pull: { (subject: object): void } = (subject: Subject) => {
   }
 };
 
-const startReaction = (reaction: Reaction) => {
-  const outerReaction = currentReaction;
-  currentReaction = reaction;
-  reaction();
-  currentReaction = outerReaction;
+/**
+ * Internal.
+ */
+const pushSweepers = (reaction: Reaction) => {
+  if (sweepersSymbol in reaction) {
+    for (let i = 0; i < reaction[sweepersSymbol].length; i++) {
+      const sweeper = reaction[sweepersSymbol][i]!;
+      if (sweeper[colorSymbol] === cleanSymbol) {
+        sweeper[colorSymbol] = checkSymbol;
+        pushSweepers(sweeper);
+      }
+    }
+  }
 };
 
-const stopReaction = (reaction: Reaction) => {
+const push: { (subject: object): void } = (subject: Subject) => {
+  if (pullersSymbol in subject) {
+    for (let i = 0; i < subject[pullersSymbol].length; i++) {
+      const puller = subject[pullersSymbol][i]!;
+      if (colorSymbol in puller) {
+        delete puller[colorSymbol];
+        pushSweepers(puller);
+      }
+    }
+  }
+};
+
+const sweep = (reaction: Reaction) => {
+  if (currentReaction) {
+    if (sweepersSymbol in reaction) {
+      reaction[sweepersSymbol].push(currentReaction);
+    } else {
+      reaction[sweepersSymbol] = [currentReaction];
+    }
+    if (sweepeesSymbol in currentReaction) {
+      currentReaction[sweepeesSymbol].push(reaction);
+    } else {
+      currentReaction[sweepeesSymbol] = [reaction];
+    }
+  }
+
+  if (reaction[colorSymbol] === cleanSymbol) {
+    return;
+  }
+
+  if (reaction[colorSymbol] === checkSymbol) {
+    // In this case we don't know if the reaction needs to be run, but by
+    // recursively calling `sweep` for sweepees, we'll eventually know one way
+    // or the other.
+    for (let i = 0; i < reaction[sweepeesSymbol]!.length; i++) {
+      sweep(reaction[sweepeesSymbol]![i]!);
+      if (!(colorSymbol in reaction)) {
+        break;
+      }
+    }
+    // If the reaction is still not dirty, this means we never broke out of the
+    // loop above and all the sweepees are now clean, and so `reaction` is clean
+    // too.
+    if (colorSymbol in reaction) {
+      reaction[colorSymbol] = cleanSymbol;
+      return;
+    }
+  }
+
   if (puleesSymbol in reaction) {
     for (let i = 0; i < reaction[puleesSymbol].length; i++) {
       const pulee = reaction[puleesSymbol][i]!;
@@ -58,17 +122,26 @@ const stopReaction = (reaction: Reaction) => {
     }
     delete reaction[puleesSymbol];
   }
-};
-
-const push: { (subject: object): void } = (subject: Subject) => {
-  if (pullersSymbol in subject) {
-    const pullersCopy = [...subject[pullersSymbol]];
-    for (let i = 0; i < pullersCopy.length; i++) {
-      const puller = pullersCopy[i]!;
-      stopReaction(puller);
-      startReaction(puller);
+  if (sweepeesSymbol in reaction) {
+    for (let i = 0; i < reaction[sweepeesSymbol].length; i++) {
+      const sweepee = reaction[sweepeesSymbol][i]!;
+      const sweepers: Reaction[] = sweepee[sweepersSymbol]!;
+      if (sweepers.length === 1) {
+        delete sweepee[sweepersSymbol];
+      } else {
+        const swap = sweepers.indexOf(reaction);
+        sweepers[swap] = sweepers[sweepers.length - 1]!;
+        sweepers.pop();
+      }
     }
+    delete reaction[sweepeesSymbol];
   }
+
+  const outerReaction = currentReaction;
+  currentReaction = reaction;
+  reaction[colorSymbol] = cleanSymbol;
+  reaction();
+  currentReaction = outerReaction;
 };
 
 //
@@ -93,29 +166,22 @@ const createSignal = <Value>(
   ];
 };
 
-// We have to return dispose handle in addition to accessor.
 const createMemo = <Value>(
   get: () => Value,
   initialValue?: Value
-): [() => Value, () => void] => {
-  const subject = {};
+): (() => Value) => {
   const reaction = () => {
     const newValue = get();
     if (newValue !== initialValue) {
       initialValue = newValue;
-      push(subject);
+      push(reaction);
     }
   };
-  startReaction(reaction);
-  return [
-    () => {
-      pull(subject);
-      return initialValue as Value;
-    },
-    () => {
-      stopReaction(reaction);
-    },
-  ];
+  return () => {
+    sweep(reaction);
+    pull(reaction);
+    return initialValue as Value;
+  };
 };
 
 //
@@ -124,77 +190,61 @@ const createMemo = <Value>(
 
 test("reaction", () => {
   const subject = {};
-  const callback = () => {
+  const reaction = () => {
     pull(subject);
     log("reaction");
   };
-  startReaction(callback);
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`> "reaction"`);
-  push(subject);
-  expect(readLog()).toMatchInlineSnapshot(`> "reaction"`);
-  stopReaction(callback);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`[Empty log]`);
   push(subject);
   expect(readLog()).toMatchInlineSnapshot(`[Empty log]`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> "reaction"`);
 });
 
 test("signal", () => {
   const [x, setX] = createSignal(0);
   expect(x()).toMatchInlineSnapshot(`0`);
-  startReaction(() => {
+  const reaction = () => {
     log.add(label("reaction"))(x());
-  });
+  };
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`> [reaction] 0`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`[Empty log]`);
   setX(1);
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`> [reaction] 1`);
   setX(1);
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`[Empty log]`);
 });
 
 test("memo", () => {
   const [x, setX] = createSignal(0);
-  const [memo, dispose] = createMemo(() =>
-    log.add(label("memo"))(Math.min(x() * 2, 10))
-  );
-  startReaction(() => {
+  const memo = createMemo(() => log.add(label("memo"))(Math.min(x() * 2, 10)));
+  const reaction = () => {
     log.add(label("reaction"))(memo());
-  });
+  };
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`
     > [memo] 0
     > [reaction] 0
   `);
   setX(5);
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`
     > [memo] 10
     > [reaction] 10
   `);
   setX(6);
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`> [memo] 10`);
-  dispose();
-  setX(0);
-  expect(readLog()).toMatchInlineSnapshot(`[Empty log]`);
 });
 
-test("for a diamond graph there are glitches and redundant reaction calls", () => {
-  //     a
-  //   /   \
-  //  b     c
-  //   \   /
-  //     d
-  const [a, setA] = createSignal(0);
-  const [b] = createMemo(() => a());
-  const [c] = createMemo(() => a());
-  startReaction(() => {
-    log(b() + c());
-  });
-  expect(readLog()).toMatchInlineSnapshot(`> 0`);
-  setA(1);
-  expect(readLog()).toMatchInlineSnapshot(`
-    > 1
-    > 2
-  `);
-});
-
-test("for an asymmetrical diamond graph there are glitches and redundant reaction calls", () => {
+test("for an asymmetrical diamond graph there are no glitches or redundant reaction calls", () => {
   //     a
   //   /   \
   //  b     c
@@ -202,39 +252,49 @@ test("for an asymmetrical diamond graph there are glitches and redundant reactio
   //  |     d
   //   \   /
   //     e
+  //
+  // We use this particular shape of the graph because it produces glitches with
+  // simple propagation whether we go depth-first or breadth-first.
+
   const [a, setA] = createSignal(0);
-  const [b] = createMemo(() => a());
-  const [c] = createMemo(() => a());
-  const [d] = createMemo(() => c());
-  startReaction(() => {
+  const b = createMemo(() => a());
+  const c = createMemo(() => a());
+  const d = createMemo(() => c());
+  const reaction = () => {
     log(b() + d());
-  });
+  };
+  sweep(reaction);
   expect(readLog()).toMatchInlineSnapshot(`> 0`);
   setA(1);
-  expect(readLog()).toMatchInlineSnapshot(`
-    > 1
-    > 2
-  `);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> 2`);
 });
 
 test("cyclical graph", () => {
   const [a, setA] = createSignal(2);
-  startReaction(() => {
+  const reaction = () => {
     const aValue = a();
     log.add(label("reaction"))(aValue);
     if (aValue > 0) {
       setA(aValue - 1);
     }
-  });
-  expect(readLog()).toMatchInlineSnapshot(`
-    > [reaction] 2
-    > [reaction] 1
-    > [reaction] 0
-  `);
+  };
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> [reaction] 2`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> [reaction] 1`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> [reaction] 0`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`[Empty log]`);
+
   setA(2);
-  expect(readLog()).toMatchInlineSnapshot(`
-    > [reaction] 2
-    > [reaction] 1
-    > [reaction] 0
-  `);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> [reaction] 2`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> [reaction] 1`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`> [reaction] 0`);
+  sweep(reaction);
+  expect(readLog()).toMatchInlineSnapshot(`[Empty log]`);
 });
